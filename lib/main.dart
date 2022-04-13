b2ee200e13101a46e61b5771c3a179482137ad6f
asolodchenko <asolodchenko@outlook.com>
2022-04-13 13:45:45 +0200

    drop_down_item

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: const Center(
+          child: MyStatefulWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String dropdownValue = '1';
+  String finalValue = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(finalValue.toString()),
+        DropdownButton<String>(
+          value: dropdownValue,
+          onChanged: (String? newValue) {
+            setState(() {
+              dropdownValue = newValue!;
+              finalValue = pow(int.parse(dropdownValue), 3).toString();
+            });
+          },
+          items: <String>['1', '2', '3', '4', '5', '6']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ],
+    );
+  }
+}
+
+
+
